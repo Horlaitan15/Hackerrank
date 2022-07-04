45f02b8dbfbdd0200c6c286d2ae9c0896d2ceb67
Horlaitan15 <ajanihorlaitan@gmail.com>
2022-07-04 12:05:13 +0100

    Added a few comments.

--- 30_days_of_code/dictionary.py
+++ 30_days_of_code/dictionary.py
@@ -1,12 +1,19 @@
 def solve():
+    # Creates a dictionary object to store phonebook.
     phonebook = dict()
+
+    # Iterates through the number of contacts to be stored.
     for _ in range(int(input())):
         name, phone = input().split()
         phonebook[name] = phone
 
+    # Since there is no actual number of queries, i decided to check the end of the queries with a while loop.
     while True:
         try:
+            # Takes the query name.
             query_name = input()
+
+            # Searches phonebook for the query name.
             if query_name in phonebook:
                 print(f"{query_name}={phonebook[query_name]}")
             else:
